b2141834e5f746f688755b6155632a79356261b4
ejwalters <ejwalters24@gmail.com>
2016-01-02 19:59:03 -0500

    Initial Commit

---
+++ rpg2-oop-exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpg2-oop-exercise
+//
+//  Created by Eric Walters on 1/2/16.
+//  Copyright © 2016 Eric Walters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
